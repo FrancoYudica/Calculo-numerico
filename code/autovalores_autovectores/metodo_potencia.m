3ed450f1a6de96a3c3fd107d7a00b5d2ea609965
FrancoYudica <francoyudica@gmail.com>
2023-04-05 15:53:29 -0300

    Power method and inverse power method

--- code/autovalores_autovectores/metodo_potencia.m
+++ code/autovalores_autovectores/metodo_potencia.m
@@ -1,5 +1,11 @@
 
 function metodo_potencia
+  #power_method_program();
+  inverse_power_method_program();
+  
+endfunction
+
+function power_method_program()
   
   # Power method, used for finding eigenvectors and it's eigenvalues
   A = [-10, 4
@@ -14,62 +20,80 @@ function metodo_potencia
   # Check if A * eigenvector = eigenvalue * eigenvector
   A * eigenvector
   eigenvalue * eigenvector
-  
 endfunction
 
 
-function [eigenvector, eigenvalue] = power_method(A, y)
+function inverse_power_method_program()
   
-  # The method has the recursive rule A * y = new_y. Note that it's important
-  # to always normalize the new_y vector, otherwise an overflow error could
-  # happen. Alpha is the vector that contains the per-component division of
-  # new_y and y. If new_y and y have the same direction, the values of alpha
-  # should be almost the same, that's the reason why the detention control is 
-  # implemented that way.
+  # Power method, used for finding eigenvectors and it's eigenvalues
+  A = [-10, 4
+       -4, 0];
+  y = [2; 2]; 
+  
+  inverse_A = inverse(A);
+  
+  
+  [eigenvector, eigenvalue] = power_method(inverse_A, y);
+  
+  # The eigenvectors for A and the inverse A are shared, but the eigenvalues
+  # are not, the eigenvalues of the same eigenvector of A are the reciprocal of the
+  # eigenvalues of the inverse A
+  
+  # The eigenvector is shared
+  # The eigenvalue of A is the reciprocal
+  
+  min_eigenvalue_A = 1.0 / eigenvalue
+  
+  # Prints the results of the algorithm
+  eigenvector
+  eigenvalue
+  
+  disp("Test results for inverse_A");
+  error_test = inverse_A * eigenvector - eigenvalue * eigenvector
+  
+  disp("Test min eigenvalue of A");
+  error_test = A * eigenvector - min_eigenvalue_A * eigenvector
+  
+  
+  
+endfunction
+
+function [eigenvector, eigenvalue] = power_method(A, start_y)
+  
+  # The method has the recursive rule A * y = new_y.
+  # it's using the "escalonado" approach 
+  # new_y and y. If new_y and y have the same direction
   # Note that the y value is the aproximation of the eigenvector
-  # Note that the alpha components are the eigenvalue
-  
-  n = columns(A);
   max_iterations = 100;
-  new_y = zeros(n, 1);
-  alpha = zeros(n, 1);
-  tolerance = 0.1;
+  tolerance = 0.001;
+  
+  n = columns(A);  
+  y = start_y;
   
   for iterations=0 : max_iterations
     # Recursive rule
     new_y = A * y;
     
-    # Calculates the alpha
-    alpha = component_div(new_y, y);
+    aprox_lambda = dot(new_y, new_y) / dot(new_y, y)
+    
+    # Stop control
+    if abs(A * new_y - aprox_lambda * new_y) < tolerance
+      break
+    endif
     
     # Normalizes, preventing overflow
     # Note that we can normalize because we only care about the direction of the eigenvector
     y = new_y / infinite_norm(new_y);
-    
-    # Quit test
-    if (abs(alpha(1) - alpha(2)) < tolerance)
-      break
-    endif
-    
-    
+           
   endfor
   
   # Displays the iterations and sets the return values
   iterations
   eigenvector = y;
-  eigenvalue = alpha(1);
+  eigenvalue = aprox_lambda;
   
 endfunction
 
 function [norm] = infinite_norm(x)
   norm = max(abs(x));
-endfunction
-
-function [out] = component_div(x, y)
- # Similar to dot product, but with division
- # Assumes that the vectors are in the same dimension
- out = x; 
- for i=1 : rows(x)
-   out(i) = out(i) / y(i);
- endfor
 endfunction
